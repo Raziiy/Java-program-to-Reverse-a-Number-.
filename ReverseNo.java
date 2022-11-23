c2c7e8f37b266ac7def037c68d4079236d0cd9d2
Raziya Tabassum <108030074+Raziiy@users.noreply.github.com>
2022-11-23 15:48:06 +0530

    Add files via upload

---
+++ ReverseNo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class ReverseNo
+{
+  public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter your  Number");
+  
+    int num = sc.nextInt();
+int reversed = 0;
+    
+    System.out.println("Original Number: " + num);
+
+    while(num != 0) {
+    
+      int digit = num % 10;
+      reversed = reversed * 10 + digit;
+      num /= 10;
+    }
+
+    System.out.println("Reversed Number: " + reversed);
+  }
+}
